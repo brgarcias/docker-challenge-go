63775785f852ce379d79d59b7296673e027551d7
bruno.garcia <bruno.silva@trocafone.com>
2023-10-24 10:11:24 -0300

    fix: removing http lib to reduce docker image size

--- main.go
+++ main.go
@@ -1,15 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Full Cycle Rocks!!")
-}
+import "fmt"
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Full Cycle Rocks!!")
 }
